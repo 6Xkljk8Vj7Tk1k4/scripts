b4be7b778180a323ef63e2fb65406eefe9cf5b48
6Xkljk8Vj7Tk1k4 <6Xkljk8Vj7Tk1k4@users.noreply.github.com>
2020-02-13 10:06:38 +0100

    send email using tls 465

--- sendemail.py
+++ sendemail.py
@@ -5,8 +5,9 @@
 # ./sendemail.py "subject" "email body"
 #
 
-import smtplib, sys
+import sys
 from email.mime.text import MIMEText
+from smtplib import SMTP_SSL as SMTP
 
 sender = 'sender@domain.ltd'
 recipients = ['user1@domain.ltd', 'user2@domain.ltd']
@@ -16,7 +17,8 @@ msg['Subject'] = sys.argv[1]
 msg['From'] = sender
 msg['To'] = ", ".join(recipients)
 
-s = smtplib.SMTP('mail.domain.ltd', 25)
+s = SMTP('mail.domain.ltd', 465)
+s.set_debuglevel(False)
 s.login("sender@domain.ltd","password")
 s.sendmail(sender, recipients, msg.as_string())
 s.quit()
